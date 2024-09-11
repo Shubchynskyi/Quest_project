16df6b37daea26a9749a241b80fcf676d5918f34
Shubchynskyi <shubchinskiydmitriy@gmail.com>
2024-09-12 00:15:57 +0200

    update Readme.MD remote unused scripts

--- scripts/startComposeClear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/.." || exit #if script not into root directory
-mvn clean install
-#mvn clean install -DskipTests
-
-echo "Starting docker-compose"
-docker-compose up -d
-echo "Docker-compose started"
-docker-compose logs -f
--- scripts/stopComposeAndDeleteAppImage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/.." || exit
-#docker-compose down --rmi 'local' # NO volume
-docker-compose down --rmi 'local' --volumes   # +volume
