0100d7df5ca35e30c4c8f7e79114e939f86966d3
Shubchynskyi <shubchinskiydmitriy@gmail.com>
2024-09-22 00:10:17 +0200

    test with slf4j-test

--- src/main/java/com/javarush/quest/shubchynskyi/controllers/quest_controllers/QuestCreateController.java
+++ src/main/java/com/javarush/quest/shubchynskyi/controllers/quest_controllers/QuestCreateController.java
@@ -76,8 +76,9 @@ public class QuestCreateController {
             @RequestParam(ID) String authorId,
             RedirectAttributes redirectAttributes
     ) {
-        if (validationService.processFieldErrors(bindingResult, redirectAttributes)) {
-            log.info("Validation failed for quest: {}", questDTO.getName());
+        boolean hasFieldsErrors = validationService.processFieldErrors(bindingResult, redirectAttributes);
+        if (hasFieldsErrors) {
+            log.info("Quest creation failed due to validation errors for quest: {}", questDTO.getName());
             addQuestCreationDataToRedirectAttributes(questDTO, questText, redirectAttributes);
             return REDIRECT + CREATE_QUEST;
         }
@@ -87,16 +88,15 @@ public class QuestCreateController {
             log.info("Quest created successfully with ID: {}", quest.getId());
             return REDIRECT + Route.QUEST_EDIT_ID + quest.getId();
         } catch (AppException e) {
-            log.error("Failed to create quest: {}", questDTO.getName(), e);
+            log.error("Error occurred while creating quest: {}", questDTO.getName(), e);
             addQuestCreationDataToRedirectAttributes(questDTO, questText, redirectAttributes);
             var errorLocalizedMessage = ErrorLocalizer.getExceptionLocalizedMessage(e.getMessage());
-
             if (!errorLocalizedMessage.isMessageService()) {
                 redirectAttributes.addFlashAttribute(ERROR, errorLocalizedMessage.message());
             } else {
+                log.error("Unexpected error occurred during quest creation: {}", e.getMessage());
                 redirectAttributes.addFlashAttribute(ERROR, UNEXPECTED_ERROR);
             }
-
             return REDIRECT + CREATE_QUEST;
         }
     }
--- src/test/java/com/javarush/quest/shubchynskyi/aspects/GlobalExceptionAspectIT.java
+++ src/test/java/com/javarush/quest/shubchynskyi/aspects/GlobalExceptionAspectIT.java
@@ -3,7 +3,6 @@ package com.javarush.quest.shubchynskyi.aspects;
 
 import com.javarush.quest.shubchynskyi.test_config.ConfigIT;
 import com.javarush.quest.shubchynskyi.test_config.TestExceptionService;
-import nl.altindag.log.LogCaptor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,29 +12,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GlobalExceptionAspectIT {
 
-    @Autowired
-    private TestExceptionService testExceptionService;
-
-    @Test
-    void testExceptionLoggingAspect() {
-        // Создаем LogCaptor для класса GlobalExceptionAspect
-        LogCaptor logCaptor = LogCaptor.forClass(GlobalExceptionAspect.class);
-
-        // Вызываем метод сервиса, который выбрасывает исключение
-        try {
-            testExceptionService.methodThatThrowsException();
-        } catch (Exception e) {
-            // Ожидаемое исключение, ничего не делаем
-        }
-
-        // Проверяем, что был записан лог уровня ERROR
-        Assertions.assertFalse(logCaptor.getErrorLogs().isEmpty(), "Должен быть хотя бы один лог уровня ERROR");
-
-        // Получаем записанный лог
-        String logMessage = logCaptor.getErrorLogs().get(0);
-
-        // Проверяем содержимое лога
-        Assertions.assertTrue(logMessage.contains("Exception caught in class"),
-                "Сообщение лога должно содержать 'Exception caught in class'");
-    }
+//    @Autowired
+//    private TestExceptionService testExceptionService;
+//
+//    @Test
+//    void testExceptionLoggingAspect() {
+//        // Создаем LogCaptor для класса GlobalExceptionAspect
+//        LogCaptor logCaptor = LogCaptor.forClass(GlobalExceptionAspect.class);
+//
+//        // Вызываем метод сервиса, который выбрасывает исключение
+//        try {
+//            testExceptionService.methodThatThrowsException();
+//        } catch (Exception e) {
+//            // Ожидаемое исключение, ничего не делаем
+//        }
+//
+//        // Проверяем, что был записан лог уровня ERROR
+//        Assertions.assertFalse(logCaptor.getErrorLogs().isEmpty(), "Должен быть хотя бы один лог уровня ERROR");
+//
+//        // Получаем записанный лог
+//        String logMessage = logCaptor.getErrorLogs().get(0);
+//
+//        // Проверяем содержимое лога
+//        Assertions.assertTrue(logMessage.contains("Exception caught in class"),
+//                "Сообщение лога должно содержать 'Exception caught in class'");
+//    }
 }
